269c6069763530afda1f25a8d081f179ee0d3d1e
Gagandeep Thapar <gsthapar@gmail.com>
2023-10-01 00:04:01 -0700

    adding in logging config for other files to reference

--- startrackermodel/data/CONSTANTS.py
+++ startrackermodel/data/CONSTANTS.py
@@ -5,20 +5,57 @@ List of constants to aid in development and maintain consistency
 
 startrackermodel
 """
-
-import numpy as np
-import os
 import logging
+import logging.config
+from numpy import pi as PI
+import os
 
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(funcName)s:%(lineno)d] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "default": {
+            "level": "DEBUG",
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+            # "stream": "ext://sys.stdout",  # Default is stderr
+        },
+    },
+    "loggers": {
+        "": {  # root logger
+            "handlers": ["default"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+        "my.packg": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "__main__": {  # if __name__ == '__main__'
+            "handlers": ["default"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+
+
+# Set project level defaults
 __CONST_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 # Useful files
-YBSC = os.path.join(__CONST_ROOT, "YBSC.pkl")
+YBSC_CSV = os.path.join(__CONST_ROOT, "YBSC.csv")
 
 # Constants
-RAD2DEG = 180.0 / np.pi
-DEG2RAD = np.pi / 180.0
-RAD2ARCSEC = 180.0 / np.pi * 3600
+RAD2DEG = 180.0 / PI
+DEG2RAD = PI / 180.0
+RAD2ARCSEC = 180.0 / PI * 3600
 
 # Log levels
 level_hash = {
@@ -33,3 +70,7 @@ level_hash = {
     "CRITICAL": logging.CRITICAL,
     "C": logging.CRITICAL,
 }
+
+if __name__ == "__main__":
+    logger.info("DEBUG TEST")
+    logger.info(__CONST_ROOT)
