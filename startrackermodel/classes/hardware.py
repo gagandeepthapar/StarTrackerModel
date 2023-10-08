3391c0ff729d848394a202cfc3925e7ab0093c53
Gagandeep Thapar <gsthapar@gmail.com>
2023-10-08 12:33:29 -0700

    updating hardware to support non-square focal arrays and misc import updates

--- startrackermodel/classes/hardware.py
+++ startrackermodel/classes/hardware.py
@@ -15,62 +15,66 @@ startrackermodel
 import logging
 import logging.config
 from typing import List, Dict
-import numpy as np
-import polars as pl
 
 from data import CONSTANTS
-from classes import parameter as par
+from classes.component import Component
+from classes.parameter import NormalParameter, Parameter
 
+import numpy as np
+import pandas as pd
 
-logger = logging.getLogger(__name__)
 logging.config.dictConfig(CONSTANTS.LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
 
 
-class Hardware:
+class Hardware(Component):
     """
     Hardware class
     """
 
-    def __init__(self, hardware_dict: Dict[str, par.Parameter]):
+    def __init__(self, hardware_dict: Dict[str, Parameter]):
         """
         Initialize Hardware class
         """
 
         self.focal_length = hardware_dict.get(
-            "FOCAL_LENGTH", par.NormalParameter("FOCAL_LENGTH", "mm", 24, 0)
+            "FOCAL_LENGTH", NormalParameter("FOCAL_LENGTH", "px", 24, 0)
         )
         self.focal_array = hardware_dict.get(
-            "FOCAL_ARRAY", par.NormalParameter("FOCAL_ARRAY", "px", 1024, 0)
+            "FOCAL_ARRAY_X", NormalParameter("FOCAL_ARRAY_X", "px", 1024, 0)
+        )
+        self.focal_array = hardware_dict.get(
+            "FOCAL_ARRAY_Y", NormalParameter("FOCAL_ARRAY_Y", "px", 1024, 0)
         )
         self.f_arr_eps_x = hardware_dict.get(
-            "FOCAL_ARRAY_EPS_X", par.NormalParameter("FOCAL_ARRAY_EPS_X", "px", 0, 0)
+            "FOCAL_ARRAY_EPS_X", NormalParameter("FOCAL_ARRAY_EPS_X", "px", 0, 0)
         )
         self.f_arr_eps_y = hardware_dict.get(
-            "FOCAL_ARRAY_EPS_Y", par.NormalParameter("FOCAL_ARRAY_EPS_Y", "px", 0, 0)
+            "FOCAL_ARRAY_EPS_Y", NormalParameter("FOCAL_ARRAY_EPS_Y", "px", 0, 0)
         )
 
         self.f_arr_eps_z = hardware_dict.get(
-            "FOCAL_ARRAY_EPS_Z", par.NormalParameter("FOCAL_ARRAY_EPS_Z", "px", 0, 0)
+            "FOCAL_ARRAY_EPS_Z", NormalParameter("FOCAL_ARRAY_EPS_Z", "px", 0, 0)
         )
 
         self.f_arr_phi_x = hardware_dict.get(
-            "FOCAL_ARRAY_PHI_X", par.NormalParameter("FOCAL_ARRAY_PHI_X", "deg", 0, 0)
+            "FOCAL_ARRAY_PHI_X", NormalParameter("FOCAL_ARRAY_PHI_X", "deg", 0, 0)
         )
 
         self.f_arr_theta_y = hardware_dict.get(
             "FOCAL_ARRAY_THETA_Y",
-            par.NormalParameter("FOCAL_ARRAY_THETA_Y", "deg", 0, 0),
+            NormalParameter("FOCAL_ARRAY_THETA_Y", "deg", 0, 0),
         )
 
         self.f_arr_psi_z = hardware_dict.get(
-            "FOCAL_ARRAY_PSI_Z", par.NormalParameter("FOCAL_ARRAY_PSI_Z", "deg", 0, 0)
+            "FOCAL_ARRAY_PSI_Z", NormalParameter("FOCAL_ARRAY_PSI_Z", "deg", 0, 0)
         )
 
         self.max_vis_mag = hardware_dict.get(
-            "MAX_VIS_MAG", par.NormalParameter("MAX_VIS_MAG", "mv", 10000, 0)
+            "MAX_VIS_MAG", NormalParameter("MAX_VIS_MAG", "mv", 10000, 0)
         )
 
-        self.object_list: List[par.Parameter] = [
+        self.object_list: List[Parameter] = [
             self.focal_length,
             self.focal_array,
             self.f_arr_eps_x,
