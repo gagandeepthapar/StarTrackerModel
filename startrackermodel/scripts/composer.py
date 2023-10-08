da27a5bea7d09b3c11b5436ab7ca0e6b4cdfbb1e
Gagandeep Thapar <gsthapar@gmail.com>
2023-10-08 12:39:55 -0700

    starting composer class to house all components and begin data generation and call required scripts to introduce errors

--- startrackermodel/scripts/composer.py
+++ startrackermodel/scripts/composer.py
@@ -16,25 +16,155 @@ startrackermodel
 
 import logging
 import logging.config
+from typing import List, Dict
+
 import numpy as np
-import polars as pl
+import pandas as pd
 import matplotlib.pyplot as plt
-from classes import arguments as args
 
 from data import CONSTANTS
+from classes.hardware import Hardware
+from classes.software import Software
+from classes.environment import Environment
+from classes.estimation import Estimation
+from classes.attitude import Attitude
+from classes.parameter import Parameter
+from classes.component import Component
+from classes.enums import SimType
 
 logging.config.dictConfig(CONSTANTS.LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 
 
-def create_data():
-    return
+class Composer:
+    """
+    Converts user-supplied arguments into actionable DataFrames
+    """
 
+    def __init__(
+        self,
+        hardware: Hardware,
+        software: Hardware,
+        # environment: Environment,
+        # estimation: Estimation,
+        sim_type: SimType,
+    ):
+        self.hardware = hardware
+        self.software = software
+        # self.environment = environment
+        # self.estimation = estimation
+        self.attitude = Attitude()
+        self.sim_type = sim_type
 
-def compose_arguments():
-    return
+        self.components: List[Component] = [
+            self.attitude,
+            self.hardware,
+            self.software,
+            # self.environment,
+            # self.estimation,
+        ]
+
+        self.frontend_data = pd.DataFrame()
+        self.backend_data = pd.DataFrame()
+        self.star_catalog = self.prep_star_catalog()
+
+    @staticmethod
+    def prep_star_catalog() -> pd.DataFrame:
+        star_catalog: pd.DataFrame = pd.read_pickle(CONSTANTS.YBSC_PKL)
+        star_catalog = star_catalog.drop(
+            [
+                "spectral_type_a",
+                "spectral_type_b",
+                "ascension_proper_motion",
+                "declination_proper_motion",
+            ],
+            axis=1,
+        )
+        star_catalog["UVEC_ECI"] = star_catalog.apply(
+            lambda row: Attitude.ra_dec_to_uvec(row.right_ascension, row.declination),
+            axis=1,
+        )
+        star_catalog.catalog_number = star_catalog.catalog_number.astype("int")
+        return star_catalog
+
+    def generate_data(self, num: int) -> pd.DataFrame:
+        """
+        Generate sim-specific data and store in class
+
+        Inputs:
+            num (int): number of rows to generate
+
+        Returns:
+            pd.DataFrame: Dataframe of data
+        """
+
+        data_df = pd.DataFrame()
+        match self.sim_type:
+            case SimType.MONTE_CARLO:
+                data_df = self.modulate(num)
+
+            case SimType.SENSITIVITY:
+                data_df = self.span(num)
+
+        self.frontend_data = data_df
+        return data_df
+
+    def modulate(self, num: int) -> pd.DataFrame:
+        """
+        Generate all data based on user-supplied information
+
+        Inputs:
+            num (int): number of rows to generate
+
+        Returns:
+            pd.DataFrame: Df of all columns randomized according to distribution
+        """
+        data_df = pd.concat([comp.modulate(num) for comp in self.components], axis=1)
+        return data_df
+
+    def span(self, num: int) -> pd.DataFrame:
+        """
+        Generate Sensitivity Analysis inputs
+
+        Inputs:
+            num (int): number of rows to generate
+
+        Returns:
+            pd.DataFrame: DF of Sensitivity analysis data
+        """
+        # extract all relevant parameters from child components
+        total_objects: List[Parameter] = []
+        for comp in self.components[1:]:
+            total_objects.extend(comp.object_list)
+
+        # span each parameter based on total number of parameters
+        param_num = num // len(total_objects)
+        data_frame = pd.DataFrame()
+        logger.warning(param_num)
+        for param in total_objects:
+            check_name = param.name
+            param_df = pd.DataFrame(
+                {
+                    mod_param.name: (
+                        mod_param.span(param_num)
+                        if mod_param.name is check_name
+                        else mod_param.ideal(param_num)
+                    )
+                    for mod_param in total_objects
+                }
+            )
+
+            data_frame = pd.concat([data_frame, param_df], axis=0)
+
+        data_frame.reset_index(inplace=True)
+        data_frame = pd.concat(
+            [self.attitude.modulate(len(data_frame.index)), data_frame],
+            axis=1,
+        )
+        logger.critical(data_frame)
+        return data_frame
 
 
 if __name__ == "__main__":
-    x = np.linspace(0, 1, 100)
-    y = np.linspace(0, 1, 100) * 5 + 3
+    comp = Composer(Hardware({}), Hardware({}), SimType.MONTE_CARLO)
+    logger.debug(comp.modulate(10).columns)
