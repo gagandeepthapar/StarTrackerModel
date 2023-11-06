ee0c0265a261bb83276bea93ceb17ae50c78f380
Gagandeep Thapar <gsthapar@gmail.com>
2023-11-05 17:45:29 -0800

    implementing stopping criterion to continually simulate until std of large dataset fails to change by some set tolerance

--- startrackermodel/driver.py
+++ startrackermodel/driver.py
@@ -36,7 +36,37 @@ if __name__ == "__main__":
     start_data_gen = perf_counter()  # start timer
     composer = Composer(args.sim_hw, args.sim_sw, args.sim_type)
 
+    # initialize metrics
+    dataset = pd.DataFrame()
+    dataset_std_ratio, prev_dataset_std, batch_num = 1, 1, 1
+    len_dataset = 0
+    while dataset_std_ratio > args.eps and len_dataset <= args.max_runs:
+        # generate and run model
+        composer.generate_data(args.num_runs)  # generate data
+        composer.run_model()  # run stochastic model
 
+        # compute metrics
+        dataset = pd.concat([dataset, composer.model_data], axis=0, ignore_index=True)
+        len_dataset = len(dataset.index)
+
+        # compute stopping criterion
+        dataset_std_ratio = (
+            np.abs(dataset.ANGULAR_ERROR.std() - prev_dataset_std)
+        ) / prev_dataset_std
+        prev_dataset_std = dataset.ANGULAR_ERROR.std()
+
+        # update terminal
+        logger.info(
+            "Batch\t%d (%d Scenes): %.3f +/- %.3f (%.5f STD Change)",
+            batch_num,
+            len_dataset,
+            dataset.ANGULAR_ERROR.mean(),
+            prev_dataset_std,
+            dataset_std_ratio,
+        )
+
+        # increment batch number
+        batch_num += 1
 
     end_compute = perf_counter()
     logger.info("-")
