1fcbfb975be99eb6a8f92f2266043f33258ef195
Gagandeep Thapar <gsthapar@gmail.com>
2024-01-06 11:34:30 -0800

    updating import order for pylint standard

--- startrackermodel/driver.py
+++ startrackermodel/driver.py
@@ -12,13 +12,11 @@ startrackermodel
 # pylint: disable=locally-disabled
 import logging
 import logging.config
+from time import perf_counter
+import signal
 
-from typing import Dict
-import argparse
-import json
 import numpy as np
 import pandas as pd
-from time import perf_counter
 
 from classes.plotter import Plotter
 from classes.arguments import UserArguments
@@ -28,22 +26,38 @@ from data import CONSTANTS
 logging.config.dictConfig(CONSTANTS.LOGGING_CONFIG)
 logger = logging.getLogger("driver")
 
+INTERRUPTED = False
+
+
+def signal_handler(sig, frame):
+    "Set global flag to stop compute"
+    logger.critical("Ending Simulation at End of Current Loop")
+    global INTERRUPTED
+    INTERRUPTED = True
+
+
 if __name__ == "__main__":
+    # set signal handler
+    signal.signal(signal.SIGINT, signal_handler)
+
     # parse arguments
     args = UserArguments()
 
     # Create composer
     start_data_gen = perf_counter()  # start timer
-    composer = Composer(args.sim_hw, args.sim_sw, args.sim_type)
+    composer = Composer(args.sim_hw, args.sim_sw, args.sensor, args.env, args.sim_type)
 
     # initialize metrics
     dataset = pd.DataFrame()
     dataset_std_ratio, prev_dataset_std, batch_num = 1, 1, 1
     len_dataset = 0
     while dataset_std_ratio > args.eps and len_dataset <= args.max_runs:
+        batch_start = perf_counter()
+
         # generate and run model
-        composer.generate_data(args.num_runs)  # generate data
+        composer.generate_data(args.num_runs, args.param_select)  # generate data
         composer.run_model()  # run stochastic model
+        batch_end = perf_counter()
 
         # compute metrics
         dataset = pd.concat([dataset, composer.model_data], axis=0, ignore_index=True)
@@ -51,24 +65,37 @@ if __name__ == "__main__":
 
         # compute stopping criterion
         dataset_std_ratio = (
-            np.abs(dataset.ANGULAR_ERROR.std() - prev_dataset_std)
+            np.abs(np.std((dataset.ANGULAR_ERROR)) - prev_dataset_std)
         ) / prev_dataset_std
-        prev_dataset_std = dataset.ANGULAR_ERROR.std()
+        prev_dataset_std = np.std((dataset.ANGULAR_ERROR))
 
         # update terminal
         logger.info(
-            "Batch\t%d (%d Scenes): %.3f +/- %.3f (%.5f STD Change)",
+            "Batch\t%d (%d Scenes): %.3f +/- %.3f (%.5f STD Change); %.5f Run Time",
             batch_num,
             len_dataset,
-            dataset.ANGULAR_ERROR.mean(),
+            np.mean((dataset.ANGULAR_ERROR)),
             prev_dataset_std,
             dataset_std_ratio,
+            batch_end - batch_start,
         )
+        logger.critical(
+            np.sqrt(
+                np.square(dataset.ANGULAR_ERROR.to_numpy()).sum() / len(dataset.index)
+            )
+        )
+
+        if args.save_data:
+            dataset.to_pickle(args.pklname)
+
+        if INTERRUPTED:
+            break
 
         # increment batch number
         batch_num += 1
 
     end_compute = perf_counter()
+
     logger.info("-")
     logger.info("Number of Runs [-]:\t\t%d", len_dataset)
     logger.info("Time to Simulate [sec]:\t%.5f", end_compute - start_data_gen)
@@ -83,5 +110,5 @@ if __name__ == "__main__":
 
     if args.show_plot:
         plotter = Plotter(dataset)
-        plotter.standard()
+        plotter.standard("", False)
         plotter.show()
